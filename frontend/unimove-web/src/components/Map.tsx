3d63c5d452826dd7553ce6fd223495c92a1a2495
Marcos <mgf108@alu.ua.es>
2026-03-11 15:52:16 +0100

    Creado mapa interactivo y empezada la página principal

---
+++ frontend/unimove-web/src/components/Map.tsx
@@ -0,0 +1,34 @@
+//import { useEffect } from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import '../styles/Map.css';
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
+});
+
+interface MapProps {
+    center?: [number, number];
+    zoom?: number;
+}
+
+export default function Map({ center = [38.385, -0.513], zoom = 16 }: MapProps) {
+    return (
+        <div id='map'>
+            <MapContainer
+                center={center}
+                zoom={zoom}
+                scrollWheelZoom={true}
+                style={{ width: '100%', height: '100%' }}
+            >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+            </MapContainer>
+        </div>
+    );
+}
