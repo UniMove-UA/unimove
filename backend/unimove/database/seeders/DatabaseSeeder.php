1ef44694b693001fc75f91337e93dbd4e30f27bd
iis8-ua <iis8@alu.ua.es>
2026-04-22 20:43:31 +0200

    Correcciones de integracion.

--- backend/unimove/database/seeders/DatabaseSeeder.php
+++ backend/unimove/database/seeders/DatabaseSeeder.php
@@ -67,7 +67,7 @@ class DatabaseSeeder extends Seeder
         ]);
 
         // 3. VIAJES
-        DB::table('trips')->insert([
+        DB::table('travels')->insert([
             'driver_id' => 2,
             'vehicle_id' => 1,
             'origin' => 'Plaza Central',
@@ -81,10 +81,14 @@ class DatabaseSeeder extends Seeder
 
         // 4. RESERVAS
         DB::table('bookings')->insert([
-            'trip_id' => 1,
+            'travels_id' => 1,
             'passenger_id' => 3,
             'status' => 'confirmed',
             'created_at' => Carbon::now(),
         ]);
+
+
+        // 5. GTFS
+        $this->call(GtfsSeeder::class);
     }
 }
