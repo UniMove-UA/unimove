e9fe4b18b16f23a976255e8facec8f4d39a195d2
iis8-ua <iis8@alu.ua.es>
2026-04-27 10:10:15 +0200

    Correcciones de relaciones.

--- backend/unimove/app/Models/User.php
+++ backend/unimove/app/Models/User.php
@@ -13,23 +13,4 @@ class User extends Authenticatable
 
     protected $fillable = ['name', 'email', 'correo_institucional', 'password', 'phone', 'rating_avg', 'role', 'is_university_member'];
 
-    public function vehicles() {
-        return $this->hasMany(Vehicle::class);
-    }
-
-    public function travelAsDriver() {
-        return $this->hasMany(Travel::class, 'driver_id');
-    }
-
-    public function bookings() {
-        return $this->hasMany(Booking::class, 'passenger_id');
-    }
-
-    public function reviewsReceived() {
-        return $this->hasMany(Review::class, 'reviewee_id');
-    }
-
-    public function reviewsGiven() {
-        return $this->hasMany(Review::class, 'reviewer_id');
-    }
 }
