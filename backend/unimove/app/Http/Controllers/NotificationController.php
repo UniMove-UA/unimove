dc422df0f6d289ea196a656056f99d6636c8b34b
iis8-ua <iis8@alu.ua.es>
2026-04-27 14:30:35 +0200

    Se ha corregido nombre del archivo.

--- backend/unimove/app/Http/Controllers/NotificationController.php
+++ backend/unimove/app/Http/Controllers/NotificationController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Auth;
 class NotificationController extends Controller
 {
     // GET /notifications
-    public function notification()
+    public function myNotifications()
     {
         $userId = Auth::id();
 
