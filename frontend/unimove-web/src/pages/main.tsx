30592ff3b4c09a8ec44da7f16df7f1c45fad0686
ibm61-ua <ibm61@alu.ua.es>
2026-05-01 13:23:55 +0200

    cambios menores

--- frontend/unimove-web/src/pages/main.tsx
+++ frontend/unimove-web/src/pages/main.tsx
@@ -1,13 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
-import './styles/index.css'
-import App from './App.tsx'
+import Map from '../components/Map.tsx';
+import Page from '../components/Page.tsx';
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </StrictMode>
-)
+// Página principal
+export default function Main() {
+    return (
+        <Page name='inicio'>
+            <Map />
+        </Page>
+    );
+}
